626324356b8e324032dfc56cc94069de62121623
Daniel Walz <walzdan@gmail.com>
2018-03-08 03:40:29 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,7 @@
+var playlist = { 'Kanye': "Gold Digger"};
+function updatePlaylist(playlist, artistName, songTitle)
+{ 
+  return Object.assign({}, plyalist,{ [artistName]: value})
+}
+updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+delete playlist.Kanye
